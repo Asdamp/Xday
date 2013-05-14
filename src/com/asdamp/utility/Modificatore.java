18a964f999719cba074cc7602df844408166753e
Asdamp <Antonio.danilo92@gmail.com>
2013-05-14 20:59:43 +0200

    Deleted useless class

--- src/com/asdamp/utility/Modificatore.java
+++
@@ -1,7 +0,0 @@
-package com.asdamp.utility;
-
-public interface Modificatore {
-	
-	public void modifica(int y, int m, int day, int h, int min, boolean by, boolean bm,
-			boolean bDay, boolean bh, boolean bmin, String descri);
-}
